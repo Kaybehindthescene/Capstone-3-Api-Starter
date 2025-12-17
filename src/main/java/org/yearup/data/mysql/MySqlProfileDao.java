f9a932c3196454a93ba25542f26c6b77f5338d6e
CalebShufford <cshufford@my.yearupunited.org>
2025-12-17 15:22:36 -0500

    addded the maprow method to the MySqlProfileDao

--- src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -1,14 +1,11 @@
 package org.yearup.data.mysql;
 
 import org.springframework.stereotype.Component;
-import org.yearup.models.Product;
 import org.yearup.models.Profile;
 import org.yearup.data.ProfileDao;
 
 import javax.sql.DataSource;
 import java.sql.*;
-
-import static org.yearup.data.mysql.MySqlProductDao.mapRow;
 
 @Component
 public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
@@ -18,60 +15,99 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
         super(dataSource);
     }
 
-    @Override
-    public Profile create(Profile profile)
-    {
-        String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
-                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-        try(Connection connection = getConnection())
+        @Override
+        public Profile create(Profile profile)
         {
-            PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, profile.getUserId());
-            ps.setString(2, profile.getFirstName());
-            ps.setString(3, profile.getLastName());
-            ps.setString(4, profile.getPhone());
-            ps.setString(5, profile.getEmail());
-            ps.setString(6, profile.getAddress());
-            ps.setString(7, profile.getCity());
-            ps.setString(8, profile.getState());
-            ps.setString(9, profile.getZip());
+            String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-            ps.executeUpdate();
+            try (Connection connection = getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(sql))
+            {
+                preparedStatement.setInt(1, profile.getUserId());
+                preparedStatement.setString(2, profile.getFirstName());
+                preparedStatement.setString(3, profile.getLastName());
+                preparedStatement.setString(4, profile.getPhone());
+                preparedStatement.setString(5, profile.getEmail());
+                preparedStatement.setString(6, profile.getAddress());
+                preparedStatement.setString(7, profile.getCity());
+                preparedStatement.setString(8, profile.getState());
+                preparedStatement.setString(9, profile.getZip());
 
+                preparedStatement.executeUpdate();
+                return profile;
+            }
+            catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public Profile getByUserId(int userId)
+        {
+            String sql = "SELECT user_id, first_name, last_name, phone, email, address, city, state, zip " +
+                    "FROM profiles WHERE user_id = ?";
+
+            try (Connection connection = getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(sql))
+            {
+                preparedStatement.setInt(1, userId);
+
+                try (ResultSet resultSet = preparedStatement.executeQuery())
+                {
+                    if (resultSet.next())
+                        return mapRow(resultSet);
+
+                    return null;
+                }
+            }
+            catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void update(int userId, Profile profile)
+        {
+            String sql = "UPDATE profiles " +
+                    "SET first_name = ?, last_name = ?, phone = ?, email = ?, address = ?, city = ?, state = ?, zip = ? " +
+                    "WHERE user_id = ?";
+
+            try (Connection connection = getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(sql))
+            {
+                preparedStatement.setString(1, profile.getFirstName());
+                preparedStatement.setString(2, profile.getLastName());
+                preparedStatement.setString(3, profile.getPhone());
+                preparedStatement.setString(4, profile.getEmail());
+                preparedStatement.setString(5, profile.getAddress());
+                preparedStatement.setString(6, profile.getCity());
+                preparedStatement.setString(7, profile.getState());
+                preparedStatement.setString(8, profile.getZip());
+                preparedStatement.setInt(9, userId);
+
+                preparedStatement.executeUpdate();
+            }
+            catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private Profile mapRow(ResultSet resultSet) throws SQLException
+        {
+            Profile profile = new Profile();
+            profile.setUserId(resultSet.getInt("user_id"));
+            profile.setFirstName(resultSet.getString("first_name"));
+            profile.setLastName(resultSet.getString("last_name"));
+            profile.setPhone(resultSet.getString("phone"));
+            profile.setEmail(resultSet.getString("email"));
+            profile.setAddress(resultSet.getString("address"));
+            profile.setCity(resultSet.getString("city"));
+            profile.setState(resultSet.getString("state"));
+            profile.setZip(resultSet.getString("zip"));
             return profile;
         }
-        catch (SQLException e)
-        {
-            throw new RuntimeException(e);
-        }
     }
-
-    @Override
-    public void update(int userId, Profile profile)
-    {
-        String sql = "UPDATE profiles " +
-                "SET first_name = ?, last_name = ?, phone = ?, email = ?, address = ?, city = ?, state = ?, zip = ? " +
-                "WHERE user_id = ?";
-
-        try (Connection connection = getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql))
-        {
-            ps.setString(1, profile.getFirstName());
-            ps.setString(2, profile.getLastName());
-            ps.setString(3, profile.getPhone());
-            ps.setString(4, profile.getEmail());
-            ps.setString(5, profile.getAddress());
-            ps.setString(6, profile.getCity());
-            ps.setString(7, profile.getState());
-            ps.setString(8, profile.getZip());
-            ps.setInt(9, userId);
-
-            ps.executeUpdate();
-        }
-        catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-}
